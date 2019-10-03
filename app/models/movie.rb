8412bef606e933142c904e2cdff2a62a0397a9a9
EC2 Default User <ec2-user@ip-172-31-46-98.us-east-2.compute.internal>
2019-10-03 04:12:23 +0000

    HW2.0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 
-def self.with_rating(arr)
+def self.with_rating(arr) #Function takes in array of ratings and returns filtered out values 
     if arr!=nil
         return Movie.where(:rating =>arr.keys)    
     else
@@ -8,7 +8,7 @@ def self.with_rating(arr)
     end
 end
     
-def self.all_rating()
+def self.all_rating() #returns the filter values 
     val=Movie.all.distinct.pluck('rating')
     return val
 end
